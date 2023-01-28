abe4c31778021b68a3de2ef93e86250adbe2579c
kksks3192@gmail.com <kksks3192@gmail.com>
2023-01-28 23:57:58 +0900

    bh9655 DP

--- src/main/java/bj9655.java
+++ src/main/java/bj9655.java
@@ -4,15 +4,16 @@ import java.util.StringTokenizer;
 
 public class bj9655 {
     static int N;
-    static int M;
-    static int V;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        V = Integer.parseInt(st.nextToken());
+        if(N%2==0){
+            System.out.println("CY");
+        }else if(N%2!=0){
+            System.out.println("SK");
+        }
     }
 }
