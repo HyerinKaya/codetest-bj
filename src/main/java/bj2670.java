93dd09fb6862d125f1cfd107534fd82e025f9638
kksks3192@gmail.com <kksks3192@gmail.com>
2023-01-31 23:32:11 +0900

    bh2670

--- src/main/java/bj2670.java
+++ src/main/java/bj2670.java
@@ -1,38 +1,24 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.StringTokenizer;
 
 public class bj2670 {
     static int N;
     static ArrayList<Double> list = new ArrayList<>();
     static double max;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        double[] arr = new double[n+1];
+        double[] dp = new double[n+1];
+        double max = 0;
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-
-        for(int i=0; i<N;i++){
-            String temp = new String(br.readLine());
-            Double num = Double.parseDouble(temp);
-            list.add(num);
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Double.parseDouble(br.readLine());
+            dp[i] = Math.max(arr[i], dp[i-1] * arr[i]);
+            max = Math.max(max, dp[i]);
         }
-
-        max = 0;
-        for(int i =0; i<list.size();i++){
-            findMax(i, 1);
-        }
-        System.out.println(Math.round(max * 1000) / 1000.0);
-    }
-
-    public static void findMax(int end, double pre_sum){
-        double sum = pre_sum*(list.get(end));
-        if(sum>max){
-            max = sum;
-        }
-        if(end<list.size()-1){
-            findMax(end+1, sum);
-        }
+        System.out.print(String.format("%.3f",max));
     }
 }
