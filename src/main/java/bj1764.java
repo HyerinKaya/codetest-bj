523cdb1b1dfa81bfb5e74cbc1f09a71067506678
kksks3192@gmail.com <kksks3192@gmail.com>
2023-02-02 16:09:00 +0900

    bj1764 HashMap ArrayList

--- src/main/java/bj1764.java
+++ src/main/java/bj1764.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class bj1764 {
     public static void main(String[] args) throws IOException {
@@ -12,14 +10,28 @@ public class bj1764 {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        ArrayList<String> hearlist = new ArrayList<>();
-        ArrayList<String> seelist = new ArrayList<>();
+        HashMap<String, Integer> hearmap = new HashMap<>();
+        HashMap<String, Integer> seemap = new HashMap<>();
+        ArrayList<String> answerlist = new ArrayList<>();
 
         for(int i=0; i<N;i++){
-            hearlist.add(br.readLine());
+            hearmap.put(br.readLine(), 0);
         }
-        for(int i=0; i<N;i++){
-            seelist.add(br.readLine());
+        for(int i=0; i<M;i++){
+            seemap.put(br.readLine(), 0);
+        }
+        Iterator<String> keys = hearmap.keySet().iterator();
+        while( keys.hasNext() ) {
+            String key = keys.next();
+            if(seemap.containsKey(key)){
+                answerlist.add(key);
+            }
+        }
+
+        Collections.sort(answerlist);
+        System.out.println(answerlist.size());
+        for(String name : answerlist){
+            System.out.println(name);
         }
     }
 }
