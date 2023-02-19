a9c09c823e20d5cb4380b622c58aabea9e0a0827
kksks3192@gmail.com <kksks3192@gmail.com>
2023-02-19 23:01:01 +0900

    input code sample

--- src/main/java/inputCode.java
+++ src/main/java/inputCode.java
@@ -1,2 +1,64 @@
-package PACKAGE_NAME;public class inputCode {
+import java.io.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class inputCode {
+    public static void main(String[] args) {
+
+    }
+
+    //한 줄에 두 수
+    public static void input0() throws IOException {
+        int N,M;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+    }
+
+    //여러 줄 배열
+    public static void input1() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Integer[] B = new Integer[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+
+    }
+
+    //Hashmap으로 받기
+    public static void input2() throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for(int i=0; i<N;i++){
+            map.put(br.readLine(), 0);
+        }
+    }
+
+    //여러줄 하나씩 숫자 받기
+    public static void input3() throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        StringTokenizer stnext = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(stnext.nextToken());
+    }
 }
