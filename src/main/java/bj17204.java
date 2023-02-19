a1da5f65644f3b01615aee3cf5069ad64d980d6a
kksks3192@gmail.com <kksks3192@gmail.com>
2023-02-19 23:00:27 +0900

    bj17204 graph search

--- src/main/java/bj17204.java
+++ src/main/java/bj17204.java
@@ -1,17 +1,40 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class bj17204 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        boolean[] visited = new boolean[N];
+        int next = 0;
+        int count = 0;
+        int answer = -1;
 
-        for(int i=0; i<N; i++){
-            
+
+        for (int i = 0; i < N; i++) {
+
+            StringTokenizer str = new StringTokenizer(br.readLine());
+            arr[i] = Integer.parseInt(str.nextToken());
+        }
+
+
+        while(true){
+            if(next==M){
+                answer = count;
+                System.out.println(answer);
+                break;
+            }
+            if(visited[next]==true){
+                System.out.println(answer);
+                break;
+            }
+            visited[next] = true;
+            next = arr[next];
+            count++;
         }
 
     }
