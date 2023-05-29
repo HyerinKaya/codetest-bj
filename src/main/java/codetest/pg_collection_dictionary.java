67f3031a0c8da873bbc8d6c33b0db66e744c952f
kksks3192@gmail.com <kksks3192@gmail.com>
2023-05-29 23:43:07 +0900

    pg_collection_dictionary

--- src/main/java/codetest/pg_collection_dictionary.java
+++ src/main/java/codetest/pg_collection_dictionary.java
@@ -1,2 +1,42 @@
-package codetest;public class pg_collection_dictionary {
+package codetest;
+
+import java.util.*;
+
+public class pg_collection_dictionary {
+
+    static PriorityQueue<String> pq = new PriorityQueue<>();
+    static HashMap<String, Integer> map = new HashMap();
+    static String[] arr;
+
+    public int solution(String word) {
+        int answer = 0;
+        arr = new String[]{"A", "E", "I", "O", "U"};
+
+        arrange(0, "");
+        int num = 1;
+        while(!pq.isEmpty()){
+            String str = pq.poll();
+            map.put(str, num);
+            num+=1;
+        }
+
+        answer = map.get(word);
+
+        return answer;
+    }
+
+    static void arrange(int cnt, String word){
+        if(cnt>4){
+            return;
+        }
+        for(int i=0;i<arr.length;i++){
+            StringBuilder sb = new StringBuilder();
+            sb.append(word);
+            sb.append(arr[i]);
+            String str = sb.toString();
+            pq.add(str);
+            arrange(cnt+1, str);
+        }
+    }
+
 }
